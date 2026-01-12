5c7d1b6e0ebe9cd0d2203393933d5e8e99ce532c
Bezara06 <increasescientific@gmail.com>
2026-01-12 16:18:34 +0300

    Refactor login and registration forms layout and styles

---
+++ script/script.js
@@ -0,0 +1,18 @@
+//selecting elements
+let container_l = document.querySelector(".container-l");
+let container_r = document.querySelector(".container-r");
+let clicToRegister = document.getElementById("clic-to-register");
+let clicToLogin = document.getElementById("clic-to-login");
+
+//clic link to register
+clicToRegister.onclick = function () {
+    container_r.classList.add("show");
+    container_l.classList.remove("show");
+    title.innerText = "Register";
+}
+//clic link to register
+clicToLogin.onclick = function () {
+    container_r.classList.remove("show");
+    container_l.classList.add("show");
+    title.innerText = "Login";
+}
